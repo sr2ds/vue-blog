29c78ad9cf87054521a4bb8ebd4aba1a1efa9956
David Silva <srdavidsilva@gmail.com>
2019-03-20 07:52:10 -0300

    ArticleGrid

---
+++ src/components/ArticleGrid.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="articles">
+    <ArticleCard/>
+
+    <PromotionalCard/>
+
+    <ArticleCard/>
+  </section>
+</template>
+
+<script>
+
+const ArticleCard = () => import("@/components/ArticleCard");
+const PromotionalCard = () => import("@/components/PromotionalCard");
+
+export default {
+  name: "ArticleGrid",
+  components: {
+    ArticleCard,
+    PromotionalCard
+  }
+};
+</script>
+
+<style>
+.articles {
+  margin: 5rem 0;
+  margin-top: -200px;
+}
+.articles .content p {
+  line-height: 1.9;
+  margin: 15px 0;
+}
+</style>
+
