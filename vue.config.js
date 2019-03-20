fdfd6c990ac56bfabb6579f649484fc24f2c7a60
David Silva <srdavidsilva@gmail.com>
2019-03-20 07:15:03 -0300

    hook to copy dist to cordova after build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? 'vue-blog'
-      : '/'
+  const path = require("path");
+  const shell = require('child_process').execSync;
+
+  const EventHooksPlugin = require('event-hooks-webpack-plugin');
+
+  // Build to web
+  module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ?
+      'vue-blog' :
+      '/',
+    configureWebpack: {
+      plugins: [
+        new EventHooksPlugin({
+          done: () => {
+
+            /**
+             * Copy Build to Cordova 
+             * 
+             */
+            const src = `dist`;
+            const dist = `cordova/www`;
+
+            shell(`mkdir -p ${dist}; cp -r ${src}/* ${dist}`);
+            shell('sed -ic "s/\\=\\/vue-blog\\//\\=/g" cordova/www/index.html')
+          }
+        }),
+      ]
+    },
+    outputDir: path.resolve(__dirname, "dist"),
   }
