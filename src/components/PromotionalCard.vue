4149c893b5eb12ff20fec607802ead5522be7dd8
David Silva <srdavidsilva@gmail.com>
2019-03-20 07:52:22 -0300

    PromotionalCard

---
+++ src/components/PromotionalCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="hero is-info is-bold is-small promo-block">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          <i class="fa fa-bell-o"></i> Nemo enim ipsam voluptatem quia.
+        </h1>
+        <span class="tag is-black is-medium is-rounded">Natus error sit voluptatem</span>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "PromotionalCard"
+};
+</script>
