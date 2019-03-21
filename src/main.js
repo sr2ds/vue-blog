1950c2701e0967f8db0e275b861d33e7664290f7
David Silva <srdavidsilva@gmail.com>
2019-03-21 08:21:23 -0300

    Init GitHub config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import GitHub from 'github-base'
 
 import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
+Vue.GitHub = GitHub
 
 new Vue({
   render: h => h(App),
