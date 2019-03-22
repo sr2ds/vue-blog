9243b62482654047e903067e798cd3afb11696fd
srdavidsilva@gmail.com <srdavidsilva@gmail.com>
2019-03-22 17:19:44 -0300

    Init Login Page for admin

---
+++ src/components/Admin/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">Git User Account</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <input class="input is-static" type="email" :value="ownerOfGist" readonly>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">Token Git</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <input class="input" type="password" placeholder="Password Token">
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  name: "Login",
+  data() {
+    return {
+      ownerOfGist: window.ownerOfGist || 'need set this in main.js'
+    };
+  },
+  created() {}
+};
+</script>
+<style scoped>
+.login {
+  padding: 30px;
+}
+</style>
+
