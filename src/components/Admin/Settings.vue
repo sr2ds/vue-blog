1d5f183aa392d4739fa2d2be04a40e77e8f13bdc
srdavidsilva@gmail.com <srdavidsilva@gmail.com>
2019-03-28 11:32:22 -0300

    Settings show git index file

--- src/components/Admin/Settings.vue
+++ src/components/Admin/Settings.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="container">
     <h1 class="title">Blog Basic Settings</h1>
-  
+    <nav class="panel">
+      <p class="panel-heading">{{ indexName }}</p>
+      <div class="panel-block">
+        <div class="field">
+          <div class="control">
+            <p
+              class="textarea"
+              placeholder="Write your history"
+              disabled
+              rows="10"
+            >{{ indexFileContent }}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="field is-grouped is-grouped-right">
+      <p class="control">
+        <router-link to="/" class="button is-light">Hide</router-link>
+      </p>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
-  name: "Settings"
+  name: "Settings",
+  data() {
+    return {
+      indexName: window.indexFileDescript,
+      indexFileContent: {}
+    };
+  },
+  async created() {
+    this.indexFileContent = await this.$article.getArticlesList();
+  }
 };
 </script>
 
 <style scoped>
 .container {
-    padding: 50px
+  padding: 50px;
+}
+textarea {
+  width: 100%;
 }
 </style>
